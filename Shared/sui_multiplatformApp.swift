fd948d998e373c803c837d67342007aa516a6d41
Zach Eriksen <zeriksen@clientresourcesinc.com>
2020-07-10 16:09:43 -0500

    init

---
+++ Shared/sui_multiplatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  sui_multiplatformApp.swift
+//  Shared
+//
+//  Created by CRi on 7/10/20.
+//
+
+import SwiftUI
+
+@main
+struct sui_multiplatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
